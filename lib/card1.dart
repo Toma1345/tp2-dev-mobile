90ab5da476fe1fbe99a2c38b1aa28e3d120107d2
Thomas Brossier <thomas.brossier@etu.univ-orleans.fr>
2025-03-05 10:38:15 +0100

    Avec de l'interactivité (ListTile et page detail.dart)

--- lib/card1.dart
+++ lib/card1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tp2/detail.dart';
 
 import 'package:tp2/models/task.dart';
 
@@ -24,6 +25,16 @@ class Ecran1 extends StatelessWidget {
               ),
               title: Text(tasks[index].title),
               subtitle: Text(tasks[index].tags.join(" ")),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => Detail(
+                          task: tasks[index]
+                      ),
+                  )
+                );
+              },
             ),
           ),
     );
---
+++ lib/detail.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'models/task.dart';
+
+class Detail extends StatelessWidget{
+  final Task task;
+
+  const Detail({required this.task});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Task ${task.title} detail'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text('Description : ${task.description}'),
+            Text('Tags : ${task.tags.join(" ")}')
+          ],
+        )
+      ),
+    );
+  }
+
+
+}
